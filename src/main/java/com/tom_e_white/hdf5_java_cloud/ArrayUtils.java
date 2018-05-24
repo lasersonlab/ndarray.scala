9129838583781f4fdc9518814b7fe46648449fc7
Tom White <tom@cloudera.com>
2018-05-24 17:24:33 +0100

    Convert loom to parquet. Horribly slow since loom matrix is transposed.

--- src/main/java/com/tom_e_white/hdf5_java_cloud/ArrayUtils.java
+++ src/main/java/com/tom_e_white/hdf5_java_cloud/ArrayUtils.java
@@ -28,11 +28,21 @@ public class ArrayUtils {
    * The equivalent of <code>v[i:j]</code> in numpy.
    */
   public static Array index(Variable v, long i, long j) throws InvalidRangeException, IOException {
-    List<Range> rs = new ArrayList<>();
-    rs.add(new Range(i, j - 1));  // subtract 1 since Range end point is inclusive, unlike numpy
+    return index(v, 0, i, j);
+  }
+
+  /**
+   * The equivalent of <code>v[i:j]</code> in numpy along a single dimension.
+   */
+  public static Array index(Variable v, int dimIndex, long i, long j) throws InvalidRangeException, IOException {
     int rank = v.getShape().length;
-    for (int r = 1; r < rank; r++) {
-      rs.add(new Range(v.getShape(r)));
+    List<Range> rs = new ArrayList<>();
+    for (int r = 0; r < rank; r++) {
+      if (r == dimIndex) {
+        rs.add(new Range(i, j - 1));  // subtract 1 since Range end point is inclusive, unlike numpy
+      } else {
+        rs.add(new Range(v.getShape(r)));
+      }
     }
     Section s = new Section(rs);
     return v.read(s);

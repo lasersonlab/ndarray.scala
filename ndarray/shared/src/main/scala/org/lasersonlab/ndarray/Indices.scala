ed1283471f7d407d68ec1512e146c633e847bd11
Ryan Williams <ryan.blake.williams@gmail.com>
2018-10-08 22:00:44 +0000

    rm unused io interfaces

--- ndarray/shared/src/main/scala/org/lasersonlab/ndarray/Indices.scala
+++ ndarray/shared/src/main/scala/org/lasersonlab/ndarray/Indices.scala
@@ -4,11 +4,13 @@ import cats.implicits._
 import org.lasersonlab.ndarray.Indices.Idx
 
 /**
- * Generate an N-dimensional array (ot type [[A]]) filled with N-dimensional indices (of type [[ShapeT]])
+ * Generate an N-dimensional array (ot type [[A]]) filled with N-dimensional indices (of type [[ShapeT]]; each index is
+ * an [[Idx int]])
  */
 trait Indices[A[_], ShapeT[_]] {
   type Shape = ShapeT[Idx]
   type Index = ShapeT[Idx]
+
   /**
    * Generate all lattice points in the hyperrectangle between the origin (inclusive) and a provided [[ShapeT]]
    * (exclusive)

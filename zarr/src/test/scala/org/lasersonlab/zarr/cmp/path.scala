cbf9be2575aa273becc81d9347972e3adf41b1ce
Ryan Williams <ryan.blake.williams@gmail.com>
2018-10-14 00:31:11 +0000

    fix file-order in directory comparison

--- zarr/src/test/scala/org/lasersonlab/zarr/cmp/path.scala
+++ zarr/src/test/scala/org/lasersonlab/zarr/cmp/path.scala
@@ -9,10 +9,10 @@ trait path
     Cmp {
       (l, r) ⇒
         (l.isDirectory, r.isDirectory) match {
-          case ( true,  true) ⇒ cmp(l.list.toList, r.list.toList)
-          case (false, false) ⇒ cmp(l.read,        r.read       )
-          case ( true, false) ⇒ Some(s"$l is a directory, $r is a file")
-          case (false,  true) ⇒ Some(s"$l is a file, $r is a directory")
+          case ( true,  true) ⇒ cmp(l.list.toList.sortBy(_.toString), r.list.toList.sortBy(_.toString))
+          case (false, false) ⇒ cmp(l.read              , r.read              )
+          case ( true, false) ⇒ Some(s"$l is a directory, $r is a file"       )
+          case (false,  true) ⇒ Some(s"$l is a file, $r is a directory"       )
         }
     }
 }
